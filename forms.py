3a1856b504b2bfd2d4a9a1cd245501857359bd47
nyckolas <nyckolas.work@gmail.com>
2022-08-17 23:09:26 +0300

    Add form.py

---
+++ forms.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length
+
+
+class LoginForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=5, max=25)],
+                           render_kw={'placeholder': 'Username'})
+    password = PasswordField(validators=[InputRequired(), Length(min=5, max=25)],
+                             render_kw={'placeholder': 'Password'})
+    submit = SubmitField('Submit')
