ecc154a79fbf5c48f1c82e3689516bae966444d2
nyckolas <nyckolas.work@gmail.com>
2022-08-17 23:07:19 +0300

    Update app.py refactoring

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
-from flask import Flask, render_template, url_for, redirect, request
-from olx_app.parser import parser
-from config import Configuration
-
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from flask_debugtoolbar import DebugToolbarExtension
 
-app = Flask(__name__)
-app.config.from_object(Configuration)
+from config import DevelopementConfig
 
-app.register_blueprint(parser)
+# создание экземпляра приложения
+app = Flask(__name__)
+app.config.from_object(os.environ.get('FLASK_ENV') or DevelopementConfig)
+# инициализирует расширения
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
 toolbar = DebugToolbarExtension(app)
-
-
-if __name__ == '__main__':
-    app.run()
