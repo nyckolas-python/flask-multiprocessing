0ccbc0c0a5071e89918476c669f4918646182d53
nyckolas <nyckolas.work@gmail.com>
2022-08-17 23:09:07 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,18 @@
-class Configuration(object):
+import os
+
+
+app_dir = os.path.abspath(os.path.dirname(__file__))
+
+class BaseConfig(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'A SECRET KEY'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class DevelopementConfig(BaseConfig):
     DEBUG = True
-    SECRET_KEY = 'development key'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEVELOPMENT_DATABASE_URI') or \
+        'sqlite:///db/database.sqlite'
+
+class ProductionConfig(BaseConfig):
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('PRODUCTION_DATABASE_URI') or \
+	'sqlite:///db/database.sqlite'
