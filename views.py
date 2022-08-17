e9a4b4e5ec5e5fa4caa6d76edee4be1cbe690637
nyckolas <nyckolas.work@gmail.com>
2022-08-17 23:10:37 +0300

    Add views.py

---
+++ views.py
@@ -0,0 +1,38 @@
+from app import app
+from flask import (render_template, request, redirect, \
+                    url_for, flash, jsonify)
+from flask_login import login_required, login_user, logout_user
+
+from models import User, Announcements
+from forms import LoginForm
+from utils import get_items
+
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user.check_password(form.password.data):
+            login_user(user)
+            return redirect(url_for('olx'))
+    return render_template('login.html', form=form)
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    flash("You have been logged out.")
+    return redirect(url_for('login'))
+
+
+@app.route('/olx')
+@login_required
+def olx():
+    if request.is_json:
+        if request.method == 'GET':
+            items = get_items()
+            # в самом парсере проверяем айди перед парсингом, после парсим и отдаём
+            return jsonify(items[:100])
+    return render_template('olx.html')
