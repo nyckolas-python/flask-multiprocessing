c8461cf5ec2fe7b9013eef15f8456ffa8480b003
nyckolas.python@gmail.com <nyckolas.python@gmail.com>
2022-08-18 23:13:52 +0300

    Update ProductionConfig

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 # from flask_debugtoolbar import DebugToolbarExtension
 
-from config import DevelopementConfig
+from config import DevelopementConfig, ProductionConfig
 
 # создание экземпляра приложения
 app = Flask(__name__)
-app.config.from_object(os.environ.get('FLASK_ENV') or DevelopementConfig)
+app.config.from_object(os.environ.get('FLASK_ENV') or ProductionConfig)
 # инициализирует расширения
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
