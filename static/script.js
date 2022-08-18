6285cbe51775084fa20a06fdd7fc91ba7fad3b06
nyckolas <nyckolas.work@gmail.com>
2022-08-18 21:59:21 +0300

    Update script.js

--- static/script.js
+++ static/script.js
@@ -8,8 +8,10 @@ $(document).ready(function(){
             success: function (response) {
                 $.each(response, function(index) {
                     $('.list').append(
+                        '<input "id="" type="hidden" value="">'+
                         '<div><h3>' + response[index]?.name + '</h3><h3>' + response[index]?.price + '</h3>' +
-                        '<img src="' + response[index]?.image + '" alt="image" width="250" height="auto"><br>' +
+                        (response[index]?.image != '' ? ('<img src="' + response[index]?.image + '" alt="image" width="250" height="auto"><br>') : response[index]?.image) +
+                        (response[index]?.vendor_name != '' ? ('<h3>' + response[index]?.vendor_name + '</h3><br>') : response[index]?.vendor_name)  +
                         '<button class="btn btn-danger" onClick="onClickHandler()">Delete</button><br><br></div>'
                     )
                 })
