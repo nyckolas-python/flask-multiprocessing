811e8353d9e628fac656380b7db61f91b9d18b2c
nyckolas <nyckolas.work@gmail.com>
2022-08-18 21:58:27 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,6 +3,8 @@ import requests
 from multiprocessing import Pool
 from bs4 import BeautifulSoup
 
+from models import current_user, load_user
+
 base_url = 'https://www.olx.ua'
 category_url = 'https://www.olx.ua/d/uk/nedvizhimost/kvartiry/?page='
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'}
@@ -17,7 +19,6 @@ def get_urls(url: str) -> list:
 		all_urls.append(base_url + href)
 	return all_urls
 
-
 def parse(url: str) -> dict:
 	try:
 		response = requests.get(url)
@@ -25,14 +26,24 @@ def parse(url: str) -> dict:
 			soup = BeautifulSoup(response.content, 'html.parser')
 			name = soup.select_one("h1.css-r9zjja-Text").text
 			price = soup.select_one("h3.css-okktvh-Text").text
-			image = soup.select_one("div.swiper-zoom-container img[src]").get('src')
-			return {'name': name, 'price': price, 'image': image}
+			image = ''
+			vendor_name = ''
+			if int(current_user.get_id()) == 2:
+				image = soup.select_one("div.swiper-zoom-container img[src]").get('src')
+			elif int(current_user.get_id()) == 3:
+				image = soup.select_one("div.swiper-zoom-container img[src]").get('src')
+				print(soup.select_one("h4.css-1rbjef7-Text").text)
+				vendor_name = soup.select_one("h4.css-1rbjef7-Text").text
+			else:
+				return {}
+			return {'name': name, 'price': price, 'image': image, 'vendor_name': vendor_name}
+
 	except Exception as ex:
 		print(f"{ex} in {url}")
 
 
 def get_items() -> list:
-	urls: list = get_urls(category_url + '1') + get_urls(category_url + '2')
+	urls: list = get_urls(category_url + '1') + get_urls(category_url + '2') + get_urls(category_url + '3')
 
 	try:
 		with Pool(processes=10) as p:
