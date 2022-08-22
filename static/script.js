47d3065a8043a1c3d8bd4819dd6f6d413ce11fe7
nyckolas.python@gmail.com <nyckolas.python@gmail.com>
2022-08-22 07:36:06 +0300

    Update script.js

--- static/script.js
+++ static/script.js
@@ -3,16 +3,33 @@ $(document).ready(function(){
     $('.btn').click(function(){
         $.ajax({
             url: '',
-            type: 'get',
+            type: 'GET',
             contentType: 'application/json',
             success: function (response) {
                 $.each(response, function(index) {
                     $('.list').append(
-                        '<input "id="" type="hidden" value="">'+
-                        '<div><h3>' + response[index]?.name + '</h3><h3>' + response[index]?.price + '</h3>' +
-                        (response[index]?.image != '' ? ('<img src="' + response[index]?.image + '" alt="image" width="250" height="auto"><br>') : response[index]?.image) +
-                        (response[index]?.vendor_name != '' ? ('<h3>' + response[index]?.vendor_name + '</h3><br>') : response[index]?.vendor_name)  +
-                        '<button class="btn btn-danger" onClick="onClickHandler()">Delete</button><br><br></div>'
+                        '<div class="row justify-content" style="margin: 15px;">' +
+                        '<button id="'+response[index]?.olx_id+'" data-id="'+response[index]?.olx_id+'" class="btn btn-danger" style="width: 6%;" onClick="onClickHandler(this)">Delete</button>' +
+                        '<div class="col col-lg-2 text-center" style="width: 13%; display: block">' +
+                        '<input value="'+response[index]?.olx_id+'" type="hidden" name="olx_id" id="olx_id">'+
+                        (response[index]?.image != '' ? ('<img src="' + response[index]?.image + '" alt="image" width="150" height="auto" style="margin-right: 20px">') : '<h4 style="display: contents;">#</h4>') +
+                        '</div>' +
+                        '<div class="col col-lg-2" style="width: 52%; display: block">' +
+                        '<h4 style="display: contents;">' +
+                        response[index]?.name + 
+                        '</h4>' +
+                        '</div>' +
+                        '<div class="col col-lg-2 text-center" style="width: 15%; display: block">' +
+                        '<h4 style="display: contents;">' +
+                        response[index]?.price +
+                        '</h4>' +
+                        '</div>' +
+                        '<div class="col col-lg-2 text-center" style="width: 13%; display: block">' +
+                        '<h4 style="display: contents;">' +
+                        (response[index]?.vendor_name != '' ? (response[index]?.vendor_name) : '#') +
+                        '</h4>' +
+                        '</div>' +
+                        '</div>'
                     )
                 })
             }
